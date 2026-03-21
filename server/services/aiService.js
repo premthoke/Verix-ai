cdbe17b4d33633a308b73be20cb7c86e4517cf8a
premthoke <premthoke02@gmail.com>
2026-03-22 03:22:57 +0530

    Updated AI service URL

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
 const FormData = require("form-data");
+const AI_URL = "https://verix-ai-9e57.onrender.com/detect";
 
 const detectFake = async (file) => {
   const formData = new FormData();
   formData.append("file", file.buffer, file.originalname);
 
   const response = await axios.post(
-    "https://your-ai-service.onrender.com/detect",
+    "https://verix-ai-9e57.onrender.com/detect",
     formData,
     {
       headers: formData.getHeaders(),
