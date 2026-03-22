66ec24653b94679459c16b3ce8f53d3c3913c925
premthoke <premthoke02@gmail.com>
2026-03-22 11:58:47 +0530

    Fixed port binding for Render

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ app.use(express.json());
 // Routes
 app.use("/api", uploadRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
