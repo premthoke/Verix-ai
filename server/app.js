bea5f1199cae7971adc0d6fe4fa48bccae542a5f
premthoke <premthoke02@gmail.com>
2026-03-22 11:32:47 +0530

    Backend ready for deployment

--- server/app.js
+++ server/app.js
@@ -13,6 +13,6 @@ app.use("/api", uploadRoutes);
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
