322d3dac252e87c938484654a2b674b8516ddcfd
premthoke <premthoke02@gmail.com>
2026-03-22 13:20:42 +0530

    Fixed AI service duplicate call + error handling

--- server/services/aiService.js
+++ server/services/aiService.js
@@ -1,36 +1,28 @@
 const axios = require("axios");
 const FormData = require("form-data");
+
 const AI_URL = "https://verix-ai-9e57.onrender.com/detect";
 
 const detectFake = async (file) => {
-  const formData = new FormData();
-  formData.append("file", file.buffer, file.originalname);
-
-  const response = await axios.post(
-    "https://verix-ai-9e57.onrender.com/detect",
-    formData,
-    {
-      headers: formData.getHeaders(),
-    }
-  );
-
   try {
-  const response = await axios.post(AI_URL, formData, {
-    headers: formData.getHeaders(),
-  });
+    const formData = new FormData();
+    formData.append("file", file.buffer, file.originalname);
 
-  return response.data;
+    const response = await axios.post(AI_URL, formData, {
+      headers: formData.getHeaders(),
+    });
 
-} catch (error) {
-  console.log("AI ERROR:", error.message);
+    return response.data;
 
-  // fallback
-  return {
-    confidence: 0.5,
-    result: "Unknown (AI limit reached)"
-  };
-}
-  return response.data;
+  } catch (error) {
+    console.log("AI ERROR:", error.message);
+
+    // fallback response (VERY IMPORTANT)
+    return {
+      confidence: 0.5,
+      result: "Unknown (AI limit reached)"
+    };
+  }
 };
 
 module.exports = { detectFake };
